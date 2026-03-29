9b119197a57a967a71c32b2cd3acd6e26c3b948e
Meve-Arroseres <meve.arroseres@epitech.eu>
2026-03-29 14:40:51 +0200

    feat: add prom-client dependency and include MetricsController in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
+import { MetricsController } from './metrics/metrics.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, HealthController],
+  controllers: [AppController, HealthController, MetricsController],
   providers: [AppService],
 })
 export class AppModule {}
